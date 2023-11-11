8ade4f031be8b3485f4caddd3816aafc8b42360d
morgu <morgunov.96@bk.ru>
2023-11-11 16:19:17 +0300

    Style css addead for all sections

--- DesignBureauSite/app.js
+++ DesignBureauSite/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector("#mobile-menu"); // check the id near in navbar section
 const menuLinks = document.querySelector(".navbar__menu"); // navbar-menu is a class. Access via '.' (targeting the ul section)
+const navbarLogo = document.querySelector("navbar__logo");
 
 // mobile menu display
 
@@ -10,3 +11,17 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener("click", mobileMenu);
+
+// show active menu while scroling
+
+// arrow functions
+const highlightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const elem1 = document.querySelector("#home-page");
+  const elem2 = document.querySelector("#about-page");
+  const elem3 = document.querySelector("#services-menu");
+  let scrollPos = window.ScrollY
+
+  if(window.innerWidth > 960 && scrollPos < 600)
+
+};
